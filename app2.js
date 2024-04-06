e06b7a21c3837a08100c6ecd684171e5a11b7850
Bihan Banerjee <bihanbanerjee@Bihans-MacBook-Pro.local>
2024-04-06 20:03:34 +0530

    Episode-1

---
+++ app2.js
@@ -0,0 +1,28 @@
+// Now, I am going to create a complex html tree structure and render that.
+// React element (Object) => HTML (Browser Understands)
+const parent = React.createElement(
+    "div", 
+    {id: "parent"},
+    [
+        React.createElement(
+        "div",
+        {id: "child"},
+        //React.createElement("h1", {}, "I'm in h1 tag.") // I can also have mutiple children in that case we will be using an array.
+        [React.createElement("h1", {}, "I'm in h1 tag."),
+        React.createElement("h2", {}, "I'm in h2 tag.")]
+    ),
+    React.createElement(
+        "div",
+        {id: "child"},
+        //React.createElement("h1", {}, "I'm in h1 tag.") // I can also have mutiple children in that case we will be using an array.
+        [React.createElement("h1", {}, "I'm in h1 tag."),
+        React.createElement("h2", {}, "I'm in h2 tag.")]
+    )
+]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
